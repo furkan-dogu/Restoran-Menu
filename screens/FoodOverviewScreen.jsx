da3cc2f28d88448a69ea61d9e6aceb8d5c2e21e1
Furkan Doğu <furkandogu2018@gmail.com>
2024-06-07 21:11:47 +0300

    yiyecekler category ekranlarına basıldı

--- screens/FoodOverviewScreen.jsx
+++ screens/FoodOverviewScreen.jsx
@@ -1,13 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, View } from "react-native";
+import React from "react";
+import { FOODS } from "../data/dummyData";
+import FoodItem from "../components/FoodItem";
 
 export default function FoodOverviewScreen({ route }) {
-    const categoryId = route.params.categoryId
+  const categoryId = route.params.categoryId;
+
+  const displayedFoods = FOODS.filter((item) => {
+    return item.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  const renderFoodItem = (data) => {
+    const foodItemProps = {
+        id: data.item.id,
+        title: data.item.title,
+        imageUrl: data.item.imageUrl,
+        affordability: data.item.affordability,
+        complexity: data.item.complexity
+    }
+    return <FoodItem {...foodItemProps} />
+  };
+
   return (
     <View>
-      <Text>FoodOverviewScreen - {categoryId}</Text>
+      <FlatList
+        data={displayedFoods}
+        keyExtractor={(item) => item.id}
+        renderItem={renderFoodItem}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
